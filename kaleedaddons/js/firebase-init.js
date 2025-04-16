e8951bcb8eb5e82c71e359335120676b3bc23bc0
kaled23 <ayrtabw@gmail.com>
2025-04-16 16:32:37 +0300

    Create firebase-init.js

---
+++ kaleedaddons/js/firebase-init.js
@@ -0,0 +1,11 @@
+console.log("جاري تهيئة Firebase...");
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
+console.log("تم تهيئة Firebase!");

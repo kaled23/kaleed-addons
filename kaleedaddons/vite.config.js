aa6d2c73e0c7ac8083313881820d305f83f2ab1d
kaled23 <ayrtabw@gmail.com>
2025-04-17 09:56:26 +0300

    Update vite.config.js

--- kaleedaddons/vite.config.js
+++ kaleedaddons/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+    root: 'public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: 'public/index.html',
+                admin: 'public/admin.html',
+                addons: 'public/addons.html'
+            }
+        }
+    }
 });
